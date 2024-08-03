9bef9777aa42ba97f272ebb8ac60de312d07d762
alexliap <alexandrosliapates@gmail.com>
2024-08-03 20:49:39 +0300

    added license changed toml file and added dependency in serve function

--- src/sk_serve/serve.py
+++ src/sk_serve/serve.py
@@ -1,10 +1,9 @@
-# import argparse
-# import uvicorn
-from api import SimpleAPI
 from fastapi import FastAPI
 
+from .api import SimpleAPI
 
-def serve(pipeline_path: str, model_path: str):
+
+def serve(simple_api: SimpleAPI):
     """_summary_
 
     Args:
@@ -15,14 +14,7 @@ def serve(pipeline_path: str, model_path: str):
         _type_: _description_
     """
     app = FastAPI()
-    api = SimpleAPI(pipeline_path, model_path)
+    api = simple_api
     app.include_router(api.routes)
 
     return app
-
-
-# def start_server(host: str = "localhost"):
-#     uvicorn.run("serve:app", host=host, port=8000, log_level="debug", reload=True)
-
-# if __name__ == '__main__':
-#     start_server()
