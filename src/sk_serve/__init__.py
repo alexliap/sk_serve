70c7b2ea4605756681854c4ea918adc1fcb67714
alexliap <alexandrosliapates@gmail.com>
2025-01-24 23:58:46 +0200

    deployment stuff

--- src/sk_serve/__init__.py
+++ src/sk_serve/__init__.py
@@ -1,6 +1,6 @@
-from sk_serve.api import SimpleAPI
-from sk_serve.serve import serve
+from .api import SimpleAPI
+from .serve import serve
 
 __version__ = "1.0.2"
 
-__all__ = (SimpleAPI, serve)
+__all__ = ("SimpleAPI", "serve")
